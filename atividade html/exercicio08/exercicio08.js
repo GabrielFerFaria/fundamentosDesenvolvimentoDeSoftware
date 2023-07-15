0cda494cbfe32f4d9797f5d9458ba36ae8cfd1c3
Gabriel <gabriel.fer.faria@gmail.com>
2023-07-15 14:25:45 -0400

    códigos terminados

---
+++ atividade html/exercicio08/exercicio08.js
@@ -0,0 +1,29 @@
+let botao = document.querySelector("#botao")
+botao.style.background="blue"
+botao.style.color="white"
+let estaQuebrado=false
+let contaCliques=0
+
+botao.addEventListener("mouseover",e =>{
+    if(!estaQuebrado===false){
+        botao.style.background="green" 
+        botao.style.color="white"
+    }
+
+})
+
+botao.addEventListener("mouseout",e =>{
+    if(!estaQuebrado===false){
+        botao.style.background="blue" 
+    }
+
+})
+
+botao.addEventListener("click", e=>{
+    contaCliques++
+    if(contaCliques>=10){
+        botao.style.background="red"
+        botao.innerHTML="Quebrei"
+        estaQuebrado=true
+    }
+})
